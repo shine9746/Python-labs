fe04d16a3e909bf075ad38c85aec55d11ea4ce91
Shine <94128087+shine9746@users.noreply.github.com>
2022-01-24 21:24:23 +0530

    Add files via upload

---
+++ Python lab-1/python-lab-1-100.py
@@ -0,0 +1,6 @@
+
+name = input("Enter your name")
+age =int(input("Enter your age")) #integer only
+birth = 2021-age
+future = birth+100 #current age + after 100 days
+print("Hello"+name+",you will become 100 years old on"+ " " + str(future))  
