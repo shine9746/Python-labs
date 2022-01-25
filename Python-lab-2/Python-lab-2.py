b6b267ca1f82dde08d43b0a9bd0d10b46ffcb037
Shine <94128087+shine9746@users.noreply.github.com>
2022-01-25 11:18:43 +0530

    Add files via upload

---
+++ Python-lab-2/Python-lab-2.py
@@ -0,0 +1,12 @@
+#catch number from user and shows messages according to whether the number is even or odd
+
+name = input("Enter user name")
+
+randomnumber = int(input("Enter a number"))
+
+operation = randomnumber % 2
+
+if operation == 0:
+    print("Hello"+ " " +name+",Congratulations you are a champion!!!") #Even number
+else:
+    print("Hello"+ " " + name+",Oops!!! Better luck next time.") #Odd number
