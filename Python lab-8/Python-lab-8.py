e2c7fcf0d305baa2f2353a63457221c1cf9f7970
Shine <94128087+shine9746@users.noreply.github.com>
2022-02-01 20:48:47 +0530

    Add files via upload

---
+++ Python lab-8/Python-lab-8.py
@@ -0,0 +1,21 @@
+import random
+
+userinput = int(input("Enter a number : "))
+      
+userguess= random.randint(1, 10)
+
+
+while userguess != userinput and userguess != "break":
+    userguess = int(input("Enter a guess between 1 to 9 : "))
+
+    if userguess == "break":
+        break
+
+
+    if userguess < userinput:
+        print("Too low...")
+    elif userguess > userinput:
+        print("Too high...")
+    else:
+        print("Exactly right!!!")
+      
