bc6d1637e7e0d4d22d1eb98e789d92bbe7def926
Shine <94128087+shine9746@users.noreply.github.com>
2022-02-01 12:19:39 +0530

    Update python lab-6.py

--- python lab-6/python lab-6.py
+++ python lab-6/python lab-6.py
@@ -1,6 +1,6 @@
 a = input("Enter a word : ")
 
-if(a == a[:: -1]):     #reversedvalue
+if(a.casefold() == a[:: -1].casefold()):     #reversedvalue
     print("It is a palindrome")
 else:
     print("It is not a palindrome")
